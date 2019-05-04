e187f391f252e9ec2d88553029a5fa0782eed733
Artyom <UTF-16@yandex.ru>
2019-05-04 13:37:23 +0300

    init tabs

---
+++ main.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', function() {
+    "use strict";
+
+    let info = document.querySelector('.info-header'),
+        tab = document.querySelectorAll('.info-header-tab'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for(let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(e) {
+        let target = e.target;
+        if (target && target.matches('div.info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if(target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                }
+            }
+        }
+    });
+});
